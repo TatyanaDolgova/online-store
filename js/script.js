0eabd8d4b0cfb32888ca976dc50e146ee6ebc669
Tatiana Dolgova <dolgova_tatiana@internet.ru>
2022-11-20 10:36:42 +0500

    added npm json-server

--- js/script.js
+++ js/script.js
@@ -306,5 +306,9 @@ window.addEventListener("DOMContentLoaded", () => {
     }, 4000);
   }
 
+  fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
+
 });
 
