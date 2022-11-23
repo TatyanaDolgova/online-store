f26ff36b45b53a82c313346195c9c28337763c51
Tatiana Dolgova <dolgova_tatiana@internet.ru>
2022-11-23 11:20:27 +0500

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -368,12 +368,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
   }
 
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '');
+  }
 
   next.addEventListener('click', () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -395,9 +398,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
   prev.addEventListener('click', () => {
     if (offset == 0) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = deleteNotDigits(width) * (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -423,7 +426,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
       slideIndex = slideTo;
 
-      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset = deleteNotDigits(width) * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
 
